5e63dd17973c113c095ef070003956e1c561263e
Pozharov Roman <ru.roman.ps@gmail.com>
2021-01-31 23:10:01 +0400

    Добавил аргумент кол-ва знаков после запятой

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@ let getRandomInteger = function(min, max) {
   if (isNaN(min) || isNaN(max)) throw new Error('Аргумент не является числом!');
   return Math.round(Number(Math.min(min, max)) + Math.random() * Math.abs(min - max));
 }
-let getRandomFloat = function(min, max) {
+let getRandomFloat = function(min, max, signs) {
   if (isNaN(min) || isNaN(max)) throw new Error('Аргумент не является числом!');
-  return (Number(Math.min(min, max)) + Math.random() * Math.abs(min - max)).toFixed(2);
+  return (Number(Math.min(min, max)) + Math.random() * Math.abs(min - max)).toFixed(signs);
 }
 
 getRandomInteger(1, 7);
-getRandomFloat(1.3, 3.6);
+getRandomFloat(1.3, 3.6, 2);
