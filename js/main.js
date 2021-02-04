b7f23790d228e0da3df41010ff0cb600a69288b2
Pozharov Roman <ru.roman.ps@gmail.com>
2021-02-04 14:03:31 +0400

    Сгенерировал данные для задания 3

--- js/main.js
+++ js/main.js
@@ -9,5 +9,103 @@ let getRandomFloat = function(min, max, digits = 0) {
   return (Math.min(min, max) + Math.random() * Math.abs(min - max)).toFixed(digits);
 }
 
-getRandomInteger(1, 7);
-getRandomFloat(1.3, 3.6, 2);
+const AVATARS = [
+  'img/avatars/user01.png',
+  'img/avatars/user02.png',
+  'img/avatars/user03.png',
+  'img/avatars/user04.png',
+  'img/avatars/user05.png',
+  'img/avatars/user06.png',
+  'img/avatars/user07.png',
+  'img/avatars/user08.png',
+];
+
+const TITLES = [
+  'Уютная квартирка в центре города',
+  'Просторные аппартаменты в спальном районе',
+  'Большой и красивый дом с фонтаном',
+  'Небольшая студия около центрального парка',
+  'Маленькая комнатка у стадиона',
+  'Просторная квартира с окнами на церковь',
+  'Роскошные апартаменты с большими окнами',
+  'Бунгало на диком пляже',
+]
+
+const DESCRIPTIONS = [
+  'Отличное жилье в центре города',
+  'Прекрасный вид на центральный парк',
+  'Окна выходят на стадион',
+  'Рукой подать до пляжа',
+  'Вокруг прекрасный сад',
+  'Из окна видны высокие горы',
+]
+
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+]
+
+const CHECKINS = [
+  '12:00',
+  '13:00',
+  '14:00',
+]
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+]
+
+const PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+]
+
+const OFFERS_COUNT = 10;
+
+const newAds = new Array(OFFERS_COUNT);
+
+const getRandomArrayElement = function(array) {
+  return array[getRandomInteger(0, array.length - 1)];
+};
+
+const getRandomArrayList = function(array) {
+  let newArray = Array.from(array);
+  return newArray.splice(getRandomInteger(0, newArray.length-1), getRandomInteger(0, newArray.length-1))
+}
+
+const createAd = function() {
+  return {
+    author: {
+      avatar: getRandomArrayElement(AVATARS),
+    },
+    offer: {
+      titles: getRandomArrayElement(TITLES),
+      address: {x: getRandomFloat(35.65, 35.70, 2), y: getRandomFloat(139.70, 139.80, 2)},
+      price: getRandomInteger(0, 30000),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomInteger(1, 4),
+      guests: getRandomInteger(0, 3),
+      checkin: getRandomArrayElement(CHECKINS),
+      checkout: getRandomArrayElement(CHECKINS),
+      features: getRandomArrayList(FEATURES),
+      description: getRandomArrayElement(DESCRIPTIONS),
+      photos: getRandomArrayElement(PHOTOS),
+    },
+    location: {
+      x: getRandomFloat(35.65, 35.70, 2),
+      y: getRandomFloat(139.70, 139.80, 2),
+    },
+  };
+};
+
+for (let i = 0; i < OFFERS_COUNT; i++) {
+  newAds[i] = createAd();
+}
