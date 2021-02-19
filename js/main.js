1dff99045c1fc669db463d663305e3ac6593f895
Pozharov Roman <ru.roman.ps@gmail.com>
2021-02-19 12:17:27 +0400

    Правки

--- js/main.js
+++ js/main.js
@@ -3,13 +3,3 @@ import {renderAds} from './similar-ads.js'
 /* eslint-disable no-console*/
 console.log(renderAds)
 /* eslint-enable no-console*/
-
-let arr = [0, 2, 3, 5, 6, 4, 2, 0, 4, 9];
-console.log('arr: ', arr);
-let arr2 = new Set(arr);
-console.log('arr.set: ', arr2)
-console.log(Array.from(arr2))
-
-let arr3 = new Map(arr);
-console.log('arr.set: ', arr3)
-console.log(Array.from(arr3))
