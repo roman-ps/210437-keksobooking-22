d5b21e0731d1b4b1bdeddbc02604dc01701f9b10
Pozharov Roman <ru.roman.ps@gmail.com>
2021-02-22 12:47:41 +0400

    Задание 5.2 дополнение

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 import {renderAds} from './similar-ads.js';
-import './form.js';
+import {events} from './form.js';
 
 const MAP_CANVAS = document.querySelector('#map-canvas');
 MAP_CANVAS.appendChild(renderAds.childNodes[0]);
 
 /* eslint-disable no-console*/
 console.log(renderAds)
+console.log(events)
 /* eslint-enable no-console*/
